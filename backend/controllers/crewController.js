6af393da74391a777fd9a5dfad67b6f35f53e9cb
Vovursik <vovavaisik.lyz@gmail.com>
2025-06-02 00:00:52 +0300

    Сделал добавление экипажа чуть сменил дизайн

--- backend/controllers/crewController.js
+++ backend/controllers/crewController.js
@@ -2,6 +2,9 @@ import { Op } from "sequelize";
 import User from "../models/User.js";
 import Conductor from "../models/Conductor.js";
 import Driver from "../models/Driver.js";
+import Trolleybus from "../models/Trolleybus.js";
+import Crew from "../models/Crew.js";
+
 
 export const getAvailableConductors = async (req, res, next) => {
   try {
@@ -75,6 +78,74 @@ export const getAvailableDrivers = async (req, res, next) => {
 
     res.status(200).json({ drivers, total });
   } catch (error) {
+    next(error);
+  }
+};
+
+export const getAvailableTrolleybuses = async (req, res, next) => {
+  try {
+    const DEFAULT_LIMIT = 4;
+    const { page = "0", search = "" } = req.query;
+
+    const pageNumber = parseInt(page, 10);
+    const offset = pageNumber * DEFAULT_LIMIT;
+
+    const whereClause = {
+      crew_id: null,
+      ...(search && { number: { [Op.iLike]: `%${search}%` } }),
+    };
+
+    const { rows: trolleybuses, count: total } = await Trolleybus.findAndCountAll({
+      where: whereClause,
+      attributes: ["trolleybus_id", "number", "status"],
+      limit: DEFAULT_LIMIT,
+      offset,
+    });
+
+    res.status(200).json({ trolleybuses, total });
+  } catch (error) {
+    console.error("Ошибка при получении троллейбусов:", error);
+    next(error);
+  }
+};
+
+export const createCrew = async (req, res, next) => {
+  const { driverId, conductorId, trolleybusId } = req.body;
+
+  if (!driverId || !conductorId || !trolleybusId) {
+    return res.status(400).json({
+      error: "Все ID обязательны: driverId, conductorId, trolleybusId",
+    });
+  }
+
+  const transaction = await Crew.sequelize.transaction();
+
+  try {
+    const crew = await Crew.create({}, { transaction });
+
+    await Driver.update(
+      { crew_id: crew.crew_id },
+      { where: { user_id: driverId }, transaction }
+    );
+
+    await Conductor.update(
+      { crew_id: crew.crew_id },
+      { where: { user_id: conductorId }, transaction }
+    );
+
+    await Trolleybus.update(
+      { crew_id: crew.crew_id },
+      { where: { trolleybus_id: trolleybusId }, transaction }
+    );
+
+    await transaction.commit();
+
+    res.status(201).json({
+      message: "Экипаж успешно создан",
+      crewId: crew.id,
+    });
+  } catch (error) {
+    await transaction.rollback();
     next(error);
   }
 };
---
+++ backend/models/Crew.js
@@ -0,0 +1,17 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db.js';
+
+const Crew = sequelize.define('Crew', {
+  crew_id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true
+  }
+}, {
+  tableName: 'crew',
+  schema: 'public',
+  timestamps: false
+});
+
+export default Crew;
