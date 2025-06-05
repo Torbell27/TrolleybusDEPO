897669fb45b6f50b875ea69ac79498b5d714ac91
Виктор Белов <torbell1337@gmail.com>
2025-06-06 02:55:52 +0300

    Revert "Вроде сделал лан всё равно короч (#17)"
    This reverts commit 47f214a730025054e95ae824129ab76fdff72863.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,11 @@ import express from "express";
 import crewRouter from "./crewRouter.js";
 import mCrewRouter from "./maintenanceCrewRouter.js";
 import mRecordRouter from "./maintenanceRecordRouter.js";
-import shiftRouter from "./shiftRouter.js";
 
 const router = express.Router();
 
 router.use("/crew", crewRouter);
 router.use("/maintenance-crew", mCrewRouter);
 router.use("/maintenance-record", mRecordRouter);
-router.use("/shift", shiftRouter);
 
 export default router;
